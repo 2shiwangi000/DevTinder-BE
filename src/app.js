5d9ac18d79be7c5123fa43b615595933f9e58193
2shiwangi000 <2shiwangi000@gmail.com>
2026-01-03 15:20:51 +0530

    Refactor user route handling and clean up commented code

--- src/app.js
+++ src/app.js
@@ -9,6 +9,37 @@ app.listen(4000,() => {
     console.log('server is running on port 4000');
 })
 
-app.use('/', (req, res) => {
-    res.send('Hello from Express server!');
-});
+// app.use('/hello/2', (req, res) => {
+//     res.send('Hello from Hello Page 2!');
+// });
+
+// app.use('/hello', (req, res) => {
+//     res.send('Hello from Hello Page!');
+// });
+
+// app.use('/', (req, res) => {
+//     res.send('Hello from Express server!');
+// });
+
+// app.use('/helloworld', (req, res) => {
+//     res.send('Hello from Hello Page!');
+// });
+
+// app.get('/user', (req, res) => {
+//     res.send('User route');
+// });
+
+// app.post('/user', (req, res) => {
+//     res.send('User POST route');
+// });
+
+app.get('/user',(req,res,next) => {
+    console.log('user console 1')
+    next()
+},(req,res,next) => {
+    console.log('user console 2');
+    next()
+},(req,res,next) => {
+    console.log('user console 3')
+    res.send('uuser consolle 3')
+})
