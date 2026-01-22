86551b2f91e4d898cea83f9a4268fcac6493c9d5
2shiwangi000 <2shiwangi000@gmail.com>
2026-01-22 12:11:07 +0530

    feat: enhance connection request handling; add route for fetching accepted connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,14 +5,47 @@ const connectionRequests = require("../modelsOschemas/connectionRequest");
 
 //get all the pending connection requests #status:interested
 
+const safe_connections = [
+  "firstName",
+  "lastName",
+  "photo",
+  "age",
+  "gender",
+  "hobbies",
+  "about",
+];
+
 userRouter.get("/user/requests", userAuth, async (req, res) => {
   try {
     const loggedinId = req.user._id;
-    const filteredConnections = await connectionRequests.find({
-      status: "interested",
-      toUserId: loggedinId,
-    });
+    const filteredConnections = await connectionRequests
+      .find({
+        status: "interested",
+        toUserId: loggedinId,
+      })
+      .populate("fromUserId", safe_connections);
     res.send(filteredConnections);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      message: err.message,
+    });
+  }
+});
+
+userRouter.get("/user/connection", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    const filteredConnections = await connectionRequests
+      .find({
+        $or: [
+          { status: "accepted", toUserId: user._id },
+          { status: "accepted", fromUserId: user._id },
+        ],
+      })
+      .populate("fromUserId", safe_connections);
+    let data = filteredConnections.map((item) => item.fromUserId);
+    res.json({ data });
   } catch (err) {
     console.log(err);
     res.status(400).json({
